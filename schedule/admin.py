6bb749ec483ec3689dd45bdd61c2db8ca02c02f7
Vadim Egorov <v.egorov@ritg.ru>
2023-01-31 16:50:48 +0400

    admin

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from schedule.models import Timetable
+
+
+@admin.register(Timetable)
+class TimetableAdmin(admin.ModelAdmin):
+    pass
