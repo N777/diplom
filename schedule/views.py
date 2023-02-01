5a81dcb83fa5154fffea91c55a51fb53d8a045c4
N777 <Egorov_vadim2001@mail.ru>
2023-02-01 20:09:41 +0400

    Frontend

--- schedule/views.py
+++ schedule/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 from rest_framework import mixins, viewsets
+from rest_framework.filters import SearchFilter
 
 from schedule.filters import TimetableFilter
 from schedule.models import Timetable
@@ -13,5 +14,6 @@ class TimetableViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
 
     queryset = Timetable.objects.all()
     serializer_class = TimetableSerializer
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, SearchFilter)
     filterset_class = TimetableFilter
+    search_fields = ['group__name', 'room__number', 'teacher__name']
