c0d1263f392eee7ee0dd2335b5353e20b0c436f6
Vadim Egorov <v.egorov@ritg.ru>
2023-02-08 16:52:05 +0400

    test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,35 @@
-<script setup lang="ts">
-
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        { name: "qwe", age: 12 },
+        { name: "qw8", age: 15 },
+        { name: "qw7", age: 19 },
+      ],
+    };
+  },
+  methods: {
+    addUser() {
+      this.users.push({
+        name: "Rtrd",
+        age: 15,
+      });
+    },
+  },
+};
 </script>
 
 <template>
   <main>
-    Хуй
+    <div>
+      <button @click="addUser"></button>
+      <ul>
+        <li v-for="user in users" :key="user.name">
+          {{ user.name }}
+          {{ user.age }}
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
